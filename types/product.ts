c40ed4702cff751af28eab754f2380b3c76dd331
kirtirajsinh <kirtiraj3738@gmail.com>
2024-12-08 09:03:15 +0530

    add product and auth parameter types

---
+++ types/product.ts
@@ -0,0 +1,32 @@
+export type Product = {
+    id: string;
+    name: string;
+    price: number;
+    image: string[];
+    coverImage: string;
+    description: string;
+    isAvailableForPurchase: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    userAddress: string | null;
+    walletAddress: string;
+    productFiles?: {
+        fileKey: string;
+        fileName?: string | null;
+    }[];
+};
+
+export type AuthParams = {
+    signature: string;
+    payload: {
+        address: string;
+        domain: string;
+        expiration_time: string;
+        invalid_before: string;
+        issued_at: string;
+        nonce: string;
+        statement: string;
+        version: string;
+    }
+}
+
